a1f953de18fbaea0b7c31c4db2e13c249aa52062
MATSUBARA Nobutada <nobutada0507@gmail.com>
2020-11-20 12:42:26 +0900

    TEMP: add debug log for windows

--- yq/replace.bat
+++ yq/replace.bat
@@ -1,7 +1,7 @@
 FOR /F %%x IN ('pwd') DO SET ROOT=%%x
+set
 ls
 cd %BUILD_WORKSPACE_DIRECTORY%
-ls
 FOR %%x IN (@@SRC_PATHS@@) DO (
   %ROOT%\@@YQ_COMMAND@@ w -i %%x @@YAML_KEY@@ --tag '@@YAML_TAG@@' @@YAML_VALUE@@
 )
